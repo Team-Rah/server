7ab2d81f7202322ff19e38e8b0799c2b0e1c1088
Tony Duong <Tonyttd@yahoo.com>
2022-05-09 18:57:11 -0500

    Added the doctor and wolf voter check

--- helperFN/roles.js
+++ helperFN/roles.js
@@ -1,12 +1,107 @@
 module.exports = {
-  //comparing roles
-  //two objects: 1: voter {userId: role} 2: candidates {userId: role}
-  //check if votes are valid... wolves cant vote for each other...return boolean
-  //wolfCheck
 
-  //doctorCheck voter and candidate
-  //two arrays: 1: vote if role is doctor 2: player array: if player is dead...change to alive
-  //check player array is doctor and alive
-  //returning player array witht alive status
+  wolfCheck: (voters, players) => {
+    //check if a wolf voted for a wolf
+    let isValidVoting = true;
+    voters.forEach((voter, i, arr) => {
+      let voterUser = voter.voter;
+      let candidateUser = voter.candidate;
 
+      let voterRole = "";
+      let candidateRole = "";
+
+      players.forEach(player => {
+        if (player.player === voterUser) {
+          voterRole = player.role;
+        }
+        if (player.player === candidateUser) {
+          candidateRole = player.role;
+        }
+      });
+
+      if (voterRole === 'wolf' && candidateRole === 'wolf') {
+        isValidVoting = false;
+      }
+    });
+
+    return isValidVoting;
+  },
+  doctorCheck: (voters, players, userThatDies) => {
+    //return an object with the modified array of players and array of messages
+    let currentDoctor = [];
+    let healedCandidate = [];
+    let healedCandidateWithStatus = [];
+    players.forEach((player) => {
+      if (player.role === 'doctor') {
+        currentDoctor.push(player.player);
+      }
+    });
+
+    voters.forEach((voter) => {
+      currentDoctor.forEach(doc => {
+        if (doc === voter.voter) {
+          healedCandidate.push(voter.candidate);
+        }
+      })
+    })
+
+    healedCandidate.forEach((heal) => {
+      players.forEach(player => {
+        if (player.player === heal) {
+          healedCandidateWithStatus.push(player);
+        }
+      })
+    })
+
+    console.log(currentDoctor, healedCandidate, healedCandidateWithStatus);
+
+    healedCandidateWithStatus.forEach((healed,i, arr) => {
+      if (healed.status === false) {
+        healed.status = true;
+        healed.message = `Player ${healed.player} has been healed.`
+      }
+    });
+
+    console.log(currentDoctor, healedCandidate, healedCandidateWithStatus);
+
+    return healedCandidateWithStatus;
+  }
 };
+
+let sampleVoters = [
+  {
+    voter: 'cihad',
+    candidate: 'david'
+  },
+  {
+    voter: 'tony',
+    candidate: 'david'
+  },
+  {
+    voter: 'david',
+    candidate: 'tony'
+  }
+];
+
+let samplePlayers = [
+  {
+    player: 'tony',
+    status: true,
+    role: 'doctor',
+  },
+  {
+    player: 'cihad',
+    status: true,
+    role: 'wolf',
+  },
+  {
+    player: 'david',
+    status: false,
+    role: 'wolf'
+  },
+];
+
+let testOne = module.exports.wolfCheck(sampleVoters, samplePlayers);
+console.log(testOne);
+
+let testTwo = module.exports.doctorCheck(sampleVoters, samplePlayers);
