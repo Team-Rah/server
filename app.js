88e57ec9a708dc92bbfbd47a18a5d4793442369e
Tony Duong <Tonyttd@yahoo.com>
2022-05-14 10:49:11 -0500

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
     origin: [
-      "http://localhost:3000"
+      "http://localhost:3001"
     ],
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
